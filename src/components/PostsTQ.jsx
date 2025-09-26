f53aad4cbf25a32bd042483e319b3a08c7cade16
Ritik <alphaop6547@gmail.com>
2025-09-27 03:23:35 +0530

    practice mutations, query invalidation, and updates from mutation response

--- src/components/PostsTQ.jsx
+++ src/components/PostsTQ.jsx
@@ -1,19 +1,52 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const fetchPosts = () => {
+  return axios.get("http://localhost:4000/posts");
+};
+
+const addPost = (post) => {
+  return axios.post("http://localhost:4000/posts", post);
+};
+
 const PostsTQ = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const queryClient = useQueryClient();
+
   const { data, isLoading, isError, error, isFetching, refetch } = useQuery({
     queryKey: ["posts"],
-    queryFn: () => {
-      return axios.get("http://localhost:4000/posts");
-    },
+    queryFn: fetchPosts,
     // staleTime: 1000 * 30,
     // refetchInterval: 1000,
     // refetchIntervalInBackground: true,
     // cacheTime: 1000 * 60 * 10,
-    enabled: false,
+    // enabled: false,
   });
+
+  const { mutate: addPostMutation } = useMutation({
+    mutationFn: addPost,
+    onSuccess: (newData) => {
+      // queryClient.invalidateQueries({ queryKey: ["posts"] });
+      queryClient.setQueryData(["posts"], (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [...oldQueryData.data, newData.data],
+        };
+      });
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title.trim() || !body.trim()) return;
+    const post = { title, body };
+    addPostMutation(post);
+    setTitle("");
+    setBody("");
+  };
 
   if (isLoading) {
     return <div>Page is Loading...</div>;
@@ -25,7 +58,21 @@ const PostsTQ = () => {
 
   return (
     <div className="post-list">
-      <button onClick={refetch}>Fetch Posts</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          value={title}
+          placeholder="Enter post title"
+        />
+        <input
+          onChange={(e) => setBody(e.target.value)}
+          type="text"
+          value={body}
+          placeholder="Enter post body"
+        />
+        <button type="submit">Post</button>
+      </form>
       {data?.data.map((post) => {
         return (
           <Link to={`/tq-posts/${post.id}`} key={post.id}>
@@ -36,6 +83,7 @@ const PostsTQ = () => {
           </Link>
         );
       })}
+      {/* <button onClick={refetch}>Fetch Posts</button> */}
     </div>
   );
 };
